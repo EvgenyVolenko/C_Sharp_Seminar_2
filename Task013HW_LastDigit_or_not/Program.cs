cfd1097a5bb9ee0d81cbabd2995fd42a522a0401
Evgeny <evolenko@inbox.ru>
2022-11-25 00:36:23 +0300

    add task013HW

---
+++ Task013HW_LastDigit_or_not/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+// ​645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+int thirdDigit = 0;
+
+if (digit < 100) Console.Write("У числа нет третьей цифры");
+else 
+{
+    while (digit > 999)
+    {
+        digit = digit / 10;
+    }
+    thirdDigit = digit % 10; 
+    Console.WriteLine("Третья цифра = " + thirdDigit);
+}
