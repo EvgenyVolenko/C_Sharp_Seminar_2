49e5467f4929552435cb2d67b947c070377856d7
Evgeny <evolenko@inbox.ru>
2022-11-25 00:01:13 +0300

    add task010HW

---
+++ Task010HW_Semi_digit/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+//Решил сделать с проверкой трехзначности числа
+
+int digit;
+Console.Clear();
+
+while (true)
+{
+    Console.Write("Введите трехзначное число: ");
+    digit = Convert.ToInt32(Console.ReadLine());
+    if (digit > 99 && digit < 1000) break;
+    else Console.WriteLine("Вы ввели не ту цифру");
+}
+
+int semiDigit = (digit / 10) % 10;
+
+Console.WriteLine("Вторая цифра = " + semiDigit);
