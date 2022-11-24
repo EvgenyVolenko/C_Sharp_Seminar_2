0589ad33a9e6f8f7aad34e42e749b8dc0e7c3bbb
Evgeny <evolenko@inbox.ru>
2022-11-24 23:31:50 +0300

    add task011 and task012

---
+++ Task011HW_Del_semi_digit/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 11: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int numberA = new Random().Next(100, 1000); 
+Console.WriteLine("Случайное число из диапазона [100, 999] = " + numberA);
+
+int lasdDigit = numberA % 10;
+int semiDigit = numberA / 10;
+int firstDigit = numberA / 100;
+
+Console.WriteLine("Число без средней цифры = " + firstDigit + lasdDigit);
---
+++ Task012CW_Kratno/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 12: Напишите программу, которая будет принимать на вход два числа и выводить, является ли первое число кратным второму. 
+// Если число 1 не кратно числу 2, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.Write("Введите число №1 ");
+int firstDigit = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число №2 ");
+int secondDigit = Convert.ToInt32(Console.ReadLine());
+
+if (firstDigit % secondDigit == 0) Console.Write("Кратно");
+else Console.Write("не кратно, остаток " + firstDigit % secondDigit);
