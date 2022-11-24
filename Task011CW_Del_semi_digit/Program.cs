0e1e28d14f21f50148e26c674a7c7b2ad60ae784
Evgeny <evolenko@inbox.ru>
2022-11-25 01:31:11 +0300

    Tasks ok with correct 011

--- Task011CW_Del_semi_digit/Program.cs
+++ Task011CW_Del_semi_digit/Program.cs
@@ -7,7 +7,6 @@ int numberA = new Random().Next(100, 1000);
 Console.WriteLine("Случайное число из диапазона [100, 999] = " + numberA);
 
 int lasdDigit = numberA % 10;
-int semiDigit = numberA / 10;
 int firstDigit = numberA / 100;
 
 Console.WriteLine("Число без средней цифры = " + firstDigit + lasdDigit);
